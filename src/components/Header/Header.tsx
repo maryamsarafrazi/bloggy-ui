6b8c37a2cc638f9aed69f90805f5e3edce5601ec
Hadi Samadzad <h.samadzad@gmail.com>
2024-12-01 16:20:16 +0000

    Add daisyui and update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <div className="h-[72px] bg-neutral-300 flex flex-row w-full justify-between items-center px-24 py-3">
+      <div className="h-[72px] bg-neutral-100 flex flex-row w-full justify-between items-center px-24 py-3">
         <div className="flex flex-row items-center">
           <div className="mr-2">
             <img
@@ -16,44 +16,29 @@ export default function Header() {
           </div>
         </div>
         <div className="flex flex-row gap-2">
-          <div className="flex w-72 gap-4 items-center bg-white text-body-md text-neutral-400 px-4 py-1 border border-neutral-500 rounded">
-            <input
-              type="text"
-              placeholder="Search for a keyword ..."
-              className="order-2 bg-white text-body-md text-neutral-400 w-full py-1"
-            />
-
-            {/* Icon from Icon8, as an SVG */}
+          <label className="input input-bordered rounded w-72 h-10 bg-white border-neutral-300 flex items-center gap-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="20"
-              height="20"
-              viewBox="0,0,256,256"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
             >
-              <g
-                fill="#737373"
-                fill-rule="nonzero"
-                stroke="none"
-                stroke-width="1"
-                stroke-linecap="butt"
-                stroke-linejoin="miter"
-                stroke-miterlimit="10"
-                stroke-dasharray=""
-                stroke-dashoffset="0"
-                font-family="none"
-                font-weight="none"
-                font-size="none"
-                text-anchor="none"
-              >
-                <g transform="scale(5.33333,5.33333)">
-                  <path d="M20.5,6c-7.98444,0 -14.5,6.51556 -14.5,14.5c0,7.98444 6.51556,14.5 14.5,14.5c3.27316,0 6.28892,-1.10698 8.7207,-2.94922l9.36523,9.36328c0.50163,0.52248 1.24653,0.73295 1.94742,0.55024c0.70088,-0.18271 1.24823,-0.73006 1.43094,-1.43094c0.18271,-0.70088 -0.02776,-1.44578 -0.55024,-1.94742l-9.36328,-9.36523c1.84224,-2.43179 2.94922,-5.44755 2.94922,-8.7207c0,-7.98444 -6.51556,-14.5 -14.5,-14.5zM20.5,10c5.82269,0 10.5,4.67732 10.5,10.5c0,2.79571 -1.08593,5.3206 -2.85156,7.19727c-0.17187,0.12442 -0.32284,0.2754 -0.44727,0.44727c-1.87707,1.76787 -4.40346,2.85547 -7.20117,2.85547c-5.82268,0 -10.5,-4.67732 -10.5,-10.5c0,-5.82268 4.67732,-10.5 10.5,-10.5z"></path>
-                </g>
-              </g>
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
             </svg>
-          </div>
-          <button className="h-10 w-auto bg-neutral-950 hover:bg-neutral-700 text-neutral-50 text-label-lg rounded px-4">
+            <input
+              type="text"
+              className="grow"
+              placeholder="Search for a keyword ..."
+            />
+          </label>
+          <button
+            className="btn btn-sm w-auto h-10 bg-neutral-950 text-neutral-50  hover:bg-neutral-800 text-label-lg rounded"
+            data-theme="dark"
+          >
             Join newsletter
           </button>
         </div>
