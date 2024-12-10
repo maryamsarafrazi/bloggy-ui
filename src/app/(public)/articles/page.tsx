c5bf8a249eb99d317f67211cedb199596bf8b1d4
Hadi Samadzad <h.samadzad@gmail.com>
2024-12-10 21:53:21 +0000

    Organise sidebar in main layout

--- src/app/(public)/articles/page.tsx
+++ src/app/(public)/articles/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import ArticleList from "@/components/Article/ArticleList";
-import ArticleListItem from "@/components/Article/ArticleListItem";
 import ArticleSortTab from "@/components/Article/ArticleSortTab";
 import Hero from "@/components/Hero/Hero";
 
@@ -11,16 +10,23 @@ export default function Page() {
       <Hero />
       <section className="max-w-[1440px] mx-auto px-24">
         <div className="flex gap-6">
-          <div className="flex-[2] p-4 rounded-lg border border-neutral-500">
-            <div className="pb-4">
-              <ArticleSortTab />
+          <div className="flex-[2]">
+            <div className="p-4 rounded-lg border border-neutral-500">
+              <div className="pb-4">
+                <ArticleSortTab />
+              </div>
+              <ArticleList />
             </div>
-            <ArticleList />
           </div>
-
-          <div className="flex-[1] p-4 rounded-lg border border-neutral-500">
-            <h2>Sidebar</h2>
-            <p>Your sidebar content goes here.</p>
+          <div className="flex-[1]">
+            <div className="h-fit p-4 rounded-lg border border-neutral-500">
+              <h2>Sidebar</h2>
+              <p>Your sidebar content goes here.</p>
+            </div>
+            <div className="h-fit p-4 rounded-lg border border-neutral-500 mt-6">
+              <h2>Series Article</h2>
+              <p>Your sidebar content goes here.</p>
+            </div>
           </div>
         </div>
       </section>
