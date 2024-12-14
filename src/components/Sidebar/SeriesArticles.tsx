bee225e9ad83d52cf03e2ca37c6bb345ca2702fd
Hadi Samadzad <h.samadzad@gmail.com>
2024-12-14 18:55:39 +0000

    Add series article in sidebar

--- src/components/Sidebar/SeriesArticles.tsx
+++ src/components/Sidebar/SeriesArticles.tsx
@@ -2,38 +2,174 @@
 
 export default function SeriesArticles() {
   return (
-    <div className="h-fit max-h-[500px] p-4 rounded-lg border border-neutral-500">
-      <h2 className="text-title-sm text-neutral-800">Top tags</h2>
+    <div className="h-fit p-4 rounded-lg border border-neutral-500">
+      <h2 className="text-title-sm text-neutral-800">Series</h2>
       <div className="divider" />
-      <div className="flex flex-wrap gap-2 my-4 text-neutral-800">
-        <div className=" bg-neutral-200 text-label-lg py-2 px-4 rounded-full w-fit">
-          <p>design-patterns</p>
+
+      <div className="flex flex-col gap-1">
+        <div className="collapse collapse-arrow bg-neutral-100 rounded-lg">
+          <input type="checkbox" name="my-accordion-2" />
+          <div className="collapse-title text-body-md font-medium">
+            User research & Strategy (4 Parts)
+          </div>
+          <div className="collapse-content text-body-sm">
+            <ul className="flex flex-col gap-2 pl-2">
+              <li className="link">
+                <a href="">Part 1: Low-fidelity wireframes</a>
+              </li>
+              <li className="link">
+                <a href="">Part 2: Interactive prototypes with Figma</a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 3: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 4: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className=" bg-neutral-200 text-label-lg py-2 px-4 rounded-full w-fit">
-          <p>user-testing</p>
+
+        <div className="collapse collapse-arrow bg-neutral-100 rounded-lg">
+          <input type="checkbox" name="my-accordion-2" />
+          <div className="collapse-title text-body-md font-medium">
+            User research & Strategy (4 Parts)
+          </div>
+          <div className="collapse-content text-body-sm">
+            <ul className="flex flex-col gap-2 pl-2">
+              <li className="link">
+                <a href="">Part 1: Low-fidelity wireframes</a>
+              </li>
+              <li className="link">
+                <a href="">Part 2: Interactive prototypes with Figma</a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 3: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 4: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className=" bg-neutral-200 text-label-lg py-2 px-4 rounded-full w-fit">
-          <p>ideation</p>
+
+        <div className="collapse collapse-arrow bg-neutral-100 rounded-lg">
+          <input type="checkbox" name="my-accordion-2" />
+          <div className="collapse-title text-body-md font-medium">
+            User research & Strategy (4 Parts)
+          </div>
+          <div className="collapse-content text-body-sm">
+            <ul className="flex flex-col gap-2 pl-2">
+              <li className="link">
+                <a href="">Part 1: Low-fidelity wireframes</a>
+              </li>
+              <li className="link">
+                <a href="">Part 2: Interactive prototypes with Figma</a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 3: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 4: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className=" bg-neutral-200 text-label-lg py-2 px-4 rounded-full w-fit">
-          <p>navigation</p>
+
+        <div className="collapse collapse-arrow bg-neutral-100 rounded-lg">
+          <input type="checkbox" name="my-accordion-2" />
+          <div className="collapse-title text-body-md font-medium">
+            User research & Strategy (4 Parts)
+          </div>
+          <div className="collapse-content text-body-sm">
+            <ul className="flex flex-col gap-2 pl-2">
+              <li className="link">
+                <a href="">Part 1: Low-fidelity wireframes</a>
+              </li>
+              <li className="link">
+                <a href="">Part 2: Interactive prototypes with Figma</a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 3: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 4: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className=" bg-neutral-200 text-label-lg py-2 px-4 rounded-full w-fit">
-          <p>team-management</p>
+
+        <div className="collapse collapse-arrow bg-neutral-100 rounded-lg">
+          <input type="checkbox" name="my-accordion-2" />
+          <div className="collapse-title text-body-md font-medium">
+            User research & Strategy (4 Parts)
+          </div>
+          <div className="collapse-content text-body-sm">
+            <ul className="flex flex-col gap-2 pl-2">
+              <li className="link">
+                <a href="">Part 1: Low-fidelity wireframes</a>
+              </li>
+              <li className="link">
+                <a href="">Part 2: Interactive prototypes with Figma</a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 3: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 4: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className=" bg-neutral-200 text-label-lg py-2 px-4 rounded-full w-fit">
-          <p>strategy</p>
-        </div>
-        <div className=" bg-neutral-200 text-label-lg py-2 px-4 rounded-full w-fit">
-          <p>design-patterns</p>
-        </div>
-        <div className=" bg-neutral-200 text-label-lg py-2 px-4 rounded-full w-fit">
-          <p>design-process</p>
-        </div>
-        <div className=" bg-neutral-200 text-label-lg py-2 px-4 rounded-full w-fit">
-          <p>design</p>
+
+        <div className="collapse collapse-arrow bg-neutral-100 rounded-lg">
+          <input type="checkbox" name="my-accordion-2" />
+          <div className="collapse-title text-body-md font-medium">
+            User research & Strategy (4 Parts)
+          </div>
+          <div className="collapse-content text-body-sm">
+            <ul className="flex flex-col gap-2 pl-2">
+              <li className="link">
+                <a href="">Part 1: Low-fidelity wireframes</a>
+              </li>
+              <li className="link">
+                <a href="">Part 2: Interactive prototypes with Figma</a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 3: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+              <li className="link">
+                <a href="">
+                  Part 4: Testing with clickable prototypes for testing in Figma
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+
       <div className="flex justify-end">
         <button
           className="btn btn-link w-auto text-label-lg"
