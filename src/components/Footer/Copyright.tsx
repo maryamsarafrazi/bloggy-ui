6bf58639c42d69147ceeade0ca52ffb250a8dd3e
Hadi Samadzad <h.samadzad@gmail.com>
2024-12-14 19:08:21 +0000

    Add copyright component

---
+++ src/components/Footer/Copyright.tsx
@@ -0,0 +1,12 @@
+export default function Copyright() {
+  return (
+    <footer className="mt-32">
+      <div className="flex w-full h-8 bg-neutral-100 text-neutral-900 text-body-sm py-2 px-24 items-center ">
+        <p>
+          Copyright © 2024 Bloggy. All rights reserved. Feel free to share our
+          content, and we’d love it if you mention us as the source.
+        </p>
+      </div>
+    </footer>
+  );
+}
